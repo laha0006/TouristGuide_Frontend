032da08b045eba025712ec47de84429c684d6b52
Lars <laha0006@stud.kea.dk>
2024-02-18 14:24:00 +0100

    added TouristAttractionService

---
+++ src/main/java/dev/tolana/touristguide_frontend/service/TouristAttractionService.java
@@ -0,0 +1,36 @@
+package dev.tolana.touristguide_frontend.service;
+
+import dev.tolana.touristguide_frontend.model.TouristAttraction;
+import dev.tolana.touristguide_frontend.repository.TouristAttractionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TouristAttractionService {
+    private final TouristAttractionRepository repository;
+
+    public TouristAttractionService(TouristAttractionRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<TouristAttraction> getAll() {
+        return repository.getAll();
+    }
+
+    public TouristAttraction getAttraction(String name) {
+        return repository.getAttraction(name);
+    }
+
+    public TouristAttraction addAttraction(TouristAttraction attraction) {
+        return repository.addAttraction(attraction);
+    }
+
+    public TouristAttraction updateAttraction(TouristAttraction attraction) {
+        return repository.updateAttraction(attraction);
+    }
+
+    public TouristAttraction deleteAttraction(String name) {
+        return repository.deleteAttraction(name);
+    }
+}
