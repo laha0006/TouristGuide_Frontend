9523fde6d3a03ac8fb90e235d40813846074aad1
Lars <laha0006@stud.kea.dk>
2024-02-18 13:56:04 +0100

    init

---
+++ src/main/java/dev/tolana/touristguide_frontend/TouristGuideFrontendApplication.java
@@ -0,0 +1,13 @@
+package dev.tolana.touristguide_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristGuideFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristGuideFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/tolana/touristguide_frontend/TouristGuideFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tolana.touristguide_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
