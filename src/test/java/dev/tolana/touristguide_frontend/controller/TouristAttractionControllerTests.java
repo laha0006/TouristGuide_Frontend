ac12b6fe43e6c7274434d00349e171ca90a2068a
Lars <laha0006@stud.kea.dk>
2024-02-27 00:54:24 +0100

    updated controller tests

--- src/test/java/dev/tolana/touristguide_frontend/controller/TouristAttractionControllerTests.java
+++ src/test/java/dev/tolana/touristguide_frontend/controller/TouristAttractionControllerTests.java
@@ -6,6 +6,7 @@ import dev.tolana.touristguide_frontend.model.TouristAttraction;
 import dev.tolana.touristguide_frontend.repository.TouristAttractionRepository;
 import dev.tolana.touristguide_frontend.service.TouristAttractionService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,9 +19,8 @@ import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+import static org.mockito.BDDMockito.given;
 @WebMvcTest(TouristAttractionController.class)
-@AutoConfigureMockMvc(addFilters = false)
 public class TouristAttractionControllerTests {
 
     @Autowired
@@ -28,6 +28,10 @@ public class TouristAttractionControllerTests {
 
     @MockBean
     private TouristAttractionService service;
+
+    private TouristAttraction testAttraction = new TouristAttraction("Test",
+            "tested",City.COPENHAGEN,
+            List.of(Tag.FREE,Tag.PAID));
 
     @Test
     public void attractionsEndpointTest() throws Exception {
@@ -59,14 +63,26 @@ public class TouristAttractionControllerTests {
     }
 
 
-    //can't test edit Endpoint with mocks ,
-    //because tempalte processing fails,
-    //since it's not getting a proper TouristAttraction
-    //so *{name} on line 15 in editAttraction fails to process
-//    @Test
-//    public void attractionEditEndpointTest() throws Exception {
-//        mockMvc.perform(get("/attractions/DenLilleFede/edit"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("editAttraction"));
-//    }
+
+    @Test
+    public void attractionEditEndpointTest() throws Exception {
+        //    have to mock the service method calls,
+        //    getAttraction, getTags, getCities
+        //    since we're mocking the service class, this is necessary.
+        TouristAttraction attractionToEdit = new TouristAttraction("DenLilleFede",
+                "Fine dining når det er bedst.",
+                City.COPENHAGEN,
+                List.of(Tag.PAID));
+        given(service.getAttraction(ArgumentMatchers.any())).willReturn(attractionToEdit);
+        given(service.getCities()).willReturn(List.of(City.values()));
+        given(service.getTags()).willReturn(List.of(Tag.values()));
+
+        mockMvc.perform(get("/attractions/DenLilleFede/edit"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("attraction",attractionToEdit))
+                .andExpect(model().attribute("tags",List.of(Tag.values())))
+                .andExpect(model().attribute("cities",List.of(City.values())))
+                .andExpect(view().name("editAttraction"));
+    }
+
 }
