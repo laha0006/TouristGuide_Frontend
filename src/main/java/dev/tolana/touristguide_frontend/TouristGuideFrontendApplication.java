37be15884d2abfcb8c3b1af9e2cebb36e2e42812
Lars <laha0006@stud.kea.dk>
2024-03-11 18:58:50 +0100

    test database

--- src/main/java/dev/tolana/touristguide_frontend/TouristGuideFrontendApplication.java
+++ src/main/java/dev/tolana/touristguide_frontend/TouristGuideFrontendApplication.java
@@ -2,10 +2,10 @@ package dev.tolana.touristguide_frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TouristGuideFrontendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TouristGuideFrontendApplication.class, args);
     }
---
+++ src/main/java/dev/tolana/touristguide_frontend/controller/DBTestController.java
@@ -0,0 +1,35 @@
+package dev.tolana.touristguide_frontend.controller;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+
+
+@Controller
+@RequestMapping("/test")
+public class DBTestController {
+    private JdbcTemplate jdbcTemplate;
+
+    public DBTestController(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    private List<Map<String, Object>> getEmployees() {
+        List<Map<String, Object>> employees = jdbcTemplate.queryForList("SELECT * FROM emp");
+        return employees;
+    }
+
+    @GetMapping("")
+    public String test(Model model) {
+        model.addAttribute("employees",getEmployees());
+        return "test";
+    }
+
+
+}
