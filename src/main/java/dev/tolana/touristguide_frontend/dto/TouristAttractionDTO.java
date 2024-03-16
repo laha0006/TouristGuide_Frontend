d766fa2e03733e4f9b7797e22ad3773bfcda59ff
Lars <laha0006@stud.kea.dk>
2024-03-16 04:08:08 +0100

    created and implemented TouristAttractionDTO

---
+++ src/main/java/dev/tolana/touristguide_frontend/dto/TouristAttractionDTO.java
@@ -0,0 +1,55 @@
+package dev.tolana.touristguide_frontend.dto;
+
+import java.util.List;
+
+public class TouristAttractionDTO {
+    public long getAttractionID() {
+        return attractionID;
+    }
+
+    public void setAttractionID(long attractionID) {
+        this.attractionID = attractionID;
+    }
+
+    private long attractionID;
+    private String name;
+    private String description;
+    private CityDTO city;
+    private List<TagDTO> tags;
+
+
+
+    public void addTag(TagDTO tagDTO) {
+        tags.add(tagDTO);
+    }
+    public List<TagDTO> getTags() {
+        return tags;
+    }
+    public void setTags(List<TagDTO> tags) {
+        this.tags = tags;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public CityDTO getCity() {
+        return city;
+    }
+
+    public void setCity(CityDTO city) {
+        this.city = city;
+    }
+}
