896b7ca68b70f8c61a29463a0bd047567c225d4d
Lars <laha0006@stud.kea.dk>
2024-03-16 04:08:55 +0100

    updated service to reflect new DTO types

--- src/main/java/dev/tolana/touristguide_frontend/service/TouristAttractionService.java
+++ src/main/java/dev/tolana/touristguide_frontend/service/TouristAttractionService.java
@@ -1,8 +1,8 @@
 package dev.tolana.touristguide_frontend.service;
 
-import dev.tolana.touristguide_frontend.model.City;
-import dev.tolana.touristguide_frontend.model.Tag;
-import dev.tolana.touristguide_frontend.model.TouristAttraction;
+import dev.tolana.touristguide_frontend.dto.CityDTO;
+import dev.tolana.touristguide_frontend.dto.TagDTO;
+import dev.tolana.touristguide_frontend.dto.TouristAttractionDTO;
 import dev.tolana.touristguide_frontend.repository.TouristAttractionRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,35 +16,35 @@ public class TouristAttractionService {
         this.repository = repository;
     }
 
-    public List<TouristAttraction> getAll() {
+    public List<TouristAttractionDTO> getAll() {
         return repository.getAll();
     }
-
-    public TouristAttraction getAttraction(String name) {
+//
+    public TouristAttractionDTO getAttraction(String name) {
         return repository.getAttraction(name);
     }
-
-    public TouristAttraction addAttraction(TouristAttraction attraction) {
-        return repository.addAttraction(attraction);
-    }
-
-    public TouristAttraction updateAttraction(TouristAttraction attraction) {
-        return repository.updateAttraction(attraction);
-    }
-
-    public TouristAttraction deleteAttraction(String name) {
-        return repository.deleteAttraction(name);
-    }
-
-    public List<Tag> getTagsByName(String name) {
+//
+//    public TouristAttraction addAttraction(TouristAttraction attraction) {
+//        return repository.addAttraction(attraction);
+//    }
+//
+//    public TouristAttraction updateAttraction(TouristAttraction attraction) {
+//        return repository.updateAttraction(attraction);
+//    }
+//
+//    public TouristAttraction deleteAttraction(String name) {
+//        return repository.deleteAttraction(name);
+//    }
+//
+    public List<TagDTO> getTagsByName(String name) {
         return repository.getTagsByName(name);
     }
-
-    public List<Tag> getTags() {
+//
+    public List<TagDTO> getTags() {
         return repository.getTags();
     }
-
-    public List<City> getCities() {
+//
+    public List<CityDTO> getCities() {
         return repository.getCities();
     }
 }
