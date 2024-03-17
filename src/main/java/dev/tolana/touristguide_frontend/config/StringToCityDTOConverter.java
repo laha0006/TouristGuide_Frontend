bfcfcca92127be9b830cc4b4ce64d8b3e9d58628
Lars <laha0006@stud.kea.dk>
2024-03-18 00:54:29 +0100

    converter so spring can convert from from data to DTO

--- src/main/java/dev/tolana/touristguide_frontend/config/StringToCityDTOConverter.java
+++ src/main/java/dev/tolana/touristguide_frontend/config/StringToCityDTOConverter.java
@@ -10,14 +10,14 @@ import org.springframework.stereotype.Component;
 public class StringToCityDTOConverter implements Converter<String, CityDTO> {
     @Override
     public CityDTO convert(String source) {
-        System.out.println("###### SOURCE : " + source);
-        System.out.println("###### SOURCE.type : " + source.getClass().getName());
+//        System.out.println("###### SOURCE : " + source);
+//        System.out.println("###### SOURCE.type : " + source.getClass().getName());
         CityDTO cityDTO = new CityDTO();
         if(source.isEmpty() || source.isBlank()) {
             return cityDTO;
         }
         String[] values = source.split(";");
-        System.out.println("########### VALUES: "+values);
+//        System.out.println("########### VALUES: "+values);
         String name = values[0];
         long city_id = Long.parseLong(values[1]);
         cityDTO.setName(name);
