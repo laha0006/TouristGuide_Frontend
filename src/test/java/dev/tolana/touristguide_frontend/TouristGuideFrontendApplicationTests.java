5acf053a1421b14e883c70c4399c0279fa515c15
Lars <laha0006@stud.kea.dk>
2024-03-24 13:32:43 +0100

    tinkering.

--- src/test/java/dev/tolana/touristguide_frontend/TouristGuideFrontendApplicationTests.java
+++ src/test/java/dev/tolana/touristguide_frontend/TouristGuideFrontendApplicationTests.java
@@ -1,7 +1,13 @@
 package dev.tolana.touristguide_frontend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 
 @SpringBootTest
 class TouristGuideFrontendApplicationTests {
